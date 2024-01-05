61fb8b898875b7d09f37cb204643d58011d43a1a
NITINROMAN1972 <romannitin5@gmail.com>
2024-01-05 16:27:30 +0530

    Bootsrap GridView

---
+++ Grid View Bootstrap/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grid_View_Bootstrap.Startup))]
+namespace Grid_View_Bootstrap
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Grid View Bootstrap/GridVIew/GridViewBootstrap.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GridVIew_GridViewBootstrap : System.Web.UI.Page
+{
+    string connectionString = ConfigurationManager.ConnectionStrings["Ginie"].ConnectionString;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            DataTable dtResp = GetResponsibilitiesData();
+            BindGridView(dtResp);
+        }
+    }
+
+    private DataTable GetResponsibilitiesData()
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            con.Open();
+            string sql = "select MenuText, MenuParent, MenuCode, Publish from MenuBars867";
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            con.Close();
+
+            return dt;
+        }
+    }
+
+    protected void BindGridView(DataTable dtResp)
+    {
+        GridMenu.DataSource = dtResp;
+        GridMenu.DataBind();
+    }
+}
